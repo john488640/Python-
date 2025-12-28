57aa7e31c01d2cefb4aefc599cd88ec3d2b96935
John Forkens <fdmh2024@outlook.com>
2025-12-28 18:36:43 +0800

    Add files via upload

---
+++ Python_projects/2.3_string/2.3_simple_string.py
@@ -0,0 +1,30 @@
+#Project: Python Strings
+#By: John Forkens Website:  https://github.com/john488640
+#E:\Project\python\2.3_string
+#2.3 P16-17
+#This program demonstrates how to concatenate two strings using the + operator.
+
+#字符串的定义
+#字符串是一系列的字符，通常用单引号或双引号括起来。
+#可以在字符串内使用引号
+
+#EG:
+#string1 = "Hello"
+
+name = "ada lovelace"
+print (name)
+
+#tilte()方法可以获取字符串的第一个字母大写的形式
+print(name.title())
+
+#upper()方法可以将字符串全部转换为大写形式
+print(name.upper())
+
+#lower()方法可以将字符串全部转换为小写形式
+print(name.lower())
+
+#output:
+#Ada Lovelace
+#ADA LOVELACE
+#ada lovelace
+
