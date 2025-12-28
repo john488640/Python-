6df94c903d50600610a038935dcaf26a8cd40cac
John Forkens <fdmh2024@outlook.com>
2025-12-28 18:39:35 +0800

    Add files via upload

---
+++ Python_projects/2.3.3_escape_characters/2.3.3_escape_characters.py
@@ -0,0 +1,40 @@
+#Project: Escape characters in Python
+#By: John Forkens Website:  https://github.com/john488640
+#E:\Project\python\2.3.3_escape_characters
+#2.3.3 P18
+#This code is used to escape characters in Python.
+
+print("\t This is a tab")
+print("This is a new line\n")
+print("This is a backslash \\")
+print("This is a single quote '")
+print("This is a double quote \"")
+print("This is a backspace \b")
+print("This is a form feed \f")
+print("This is a carriage return \r")
+print("This is a vertical tab \v")
+print("This is a horizontal tab \h")
+
+print("EG:")
+print("language:\n\tPython\n\tJava\n\tC++\n\tJavaScript")
+
+#Output:
+#	 This is a tab
+#This is a new line
+#This is a backslash \
+#This is a single quote '
+#This is a double quote "
+#This is a backspace
+#This is a form feed
+#This is a carriage return
+#This is a vertical tab
+#This is a horizontal tab
+#EG:
+#language:
+#	Python
+#	Java
+#	C++
+#	JavaScript
+
+#Espace characters are used to separate words, sentences, or paragraphs. In Python, we can use escape characters to represent these characters.
+#转义字符用于分隔单词、句子或段落。在Python中，我们可以使用转义字符来表示这些字符。
