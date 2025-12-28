fae3a0c847e9fa83dd223e9b02cb52509cc5aa34
John Forkens <fdmh2024@outlook.com>
2025-12-28 18:33:08 +0800

    Add files via upload

---
+++ Python_projects/2.2_value_message/value_message.py
@@ -0,0 +1,19 @@
+#Project: value and message program
+#By: John Forkens Website:  https://github.com/john488640
+#E:\Project\python\2.2_value_message
+#2.2 P13
+#This program will take a value and display a message based on the value entered.
+
+message = "Hello Python World! "
+print(message)
+
+#output: Hello Python World!
+
+#变量的定义：
+#变量是存储数据的容器，可以用来保存数值、字符串、列表、字典等数据类型。变量的定义格式为：变量名 = 数据。
+#变量的注意事项：
+#1.变量只能使用字母、数字、下划线，不能以数字开头。2.可以使用下划线代替空格。3.变量名不能与关键字或函数名相同。4.变量名应简短易懂。
+# 5.变量名应全部小写（大写字母开头的变量名是常量）。6.变量名应与程序中使用的变量名保持一致。7.小心1和0，容易和l和o混淆。
+
+
+#TRACEBACK是Python的错误信息，它指出了程序执行过程中发生的错误的位置。
