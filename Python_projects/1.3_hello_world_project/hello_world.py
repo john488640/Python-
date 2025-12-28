b3d11823a1482bb22721d72a0aede79930c4c178
John Forkens <fdmh2024@outlook.com>
2025-12-28 18:29:27 +0800

    Add files via upload

---
+++ Python_projects/1.3_hello_world_project/hello_world.py
@@ -0,0 +1,8 @@
+#Project: Hello World in Python
+#By: John Forkens Website:  https://github.com/john488640
+#E:\Project\python\1.3_hello_world_project
+#1.3  P7
+#This program will print "Hello Python interpreter!" to the console.
+print("Hello Python interpreter!")
+
+#Output: Hello Python interpreter!
