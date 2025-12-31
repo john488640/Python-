714d920124c640f23f56987c55b208b0b1bdef19
John Forkens <fdmh2024@outlook.com>
2025-12-31 21:56:23 +0800

    Add files via upload

---
+++ Python_projects/2.3.5_strip_prefix/2.3.5_strip_prefix.py
@@ -0,0 +1,11 @@
+#Project: strip the prefix in Python.
+#By: John Forkens Website:  https://github.com/john488640
+#E:\Project\python\2.3.5_strip_prefix
+#2.3.5 P20
+#This program will strip the prefix from a string.
+
+baidu_url = "https://www.baidu.com"
+print(baidu_url.removeprefix("https://"))
+
+
+#output: www.baidu.com
