f34afb033248751080f80996453e3fc002abd12a
John Forkens <fdmh2024@outlook.com>
2026-02-03 18:09:57 +0800

    Add files via upload

---
+++ Python_projects/2.4_numbers/2.4_numbers&2.4.2_float_numbers&2.4.4the_underlines_of_numbers.py
@@ -0,0 +1,116 @@
+#Project: Python numbers
+#By: John Forkens Website:  https://github.com/john488640
+#E:\Project\python\2.4_number
+#2.4 P22
+#This program will print the numbers in Python
+
+#Python中的数字类型有四种：整数、浮点数、复数、布尔值。
+#整数类型：整数类型是最基本的数字类型，它表示整数值。整数类型可以是正数、负数、0。
+#浮点数类型：浮点数类型是小数点后带有小数位的数字类型。浮点数类型可以表示小数。
+#复数类型：复数类型是由实数部分和虚数部分构成的数字类型。复数类型可以表示复数。
+#布尔值类型：布尔值类型只有两个值：True和False。布尔值类型可以表示真或假。
+
+
+#integer numbers
+print("This is some integer numbers in Python ")
+print("I will print \"2+3\" ")
+print(2+3)
+print ("I will print \"3-2\" ")
+print(3-2)
+print ("I will print \"2*3\" ")
+print(2*3)
+print ("I will print \"3/2\" ")
+print(3/2)
+print ("I will print \"2^3\" ")
+print(2**3)
+print("在Python中，\"**\"指的是乘方运算符")
+
+#output:
+#I will print "2+3"
+#5
+#I will print "3-2"
+#1
+#I will print "2*3"
+#6
+#I will print "3/2"
+#1.5
+#I will print "2^3"
+#8
+#在Python中，"**"指的是乘方运算符
+
+
+#float numbers
+print("This is some float numbers in Python ")
+print("I will print \"2.5+3\" ")
+print(2.5+3)
+print ("I will print \"3.5-2\" ")
+print(3.5-2)
+print ("I will print \"2.5*3\" ")
+print(2.5*3)
+print ("I will print \"3.5/2\" ")
+print(3.5/2)
+print("在Python中，浮点数类型可以表示小数")
+
+#output:
+#I will print "2.5+3"
+#5.5
+#I will print "3.5-2"
+#1.5
+#I will print "2.5*3"
+#7.5
+#I will print "3.5/2"
+#1.75
+#在Python中，浮点数类型可以表示小数
+
+#integer numbers and float numbers
+print("This is some integer and float numbers in Python ")
+print("I will print \"2+3.5\" ")
+print(2+3.5)
+print ("I will print \"3-2.5\" ")
+print(3-2.5)
+print ("I will print \"2*3.5\" ")
+print(2*3.5)
+print ("I will print \"3/2.5\" ")
+print(3/2.5)
+print("在Python中，整数和浮点数可以混合运算")
+print("将任意两个数相除，结果总是浮点数")
+print("在其他运算中，如果一个操作数是整数，另一个操作数是浮点数，则结果将是浮点数")
+
+#output:
+#This is some integer and float numbers in Python
+#I will print "2+3.5"
+#5.5
+#I will print "3-2.5"
+#0.5
+#I will print "2*3.5"
+#7.5
+#I will print "3/2.5"
+#1.25
+#在Python中，整数和浮点数可以混合运算
+#将任意两个数相除，结果总是浮点数
+#在其他运算中，如果一个操作数是整数，另一个操作数是浮点数，则结果将是浮点数
+
+#underlines of numbers
+print("This is some underlines of numbers in Python ")
+print("I will print \"2_3\" ")
+print(2_3)
+print ("I will print \"3_2\" ")
+print(3_2)
+print ("I will print \"2_3.5\" ")
+print(2_3.5)
+print ("I will print \"3_2.5\" ")
+print(3_2.5)
+print("在Python中，下划线可以用来增强可读性")
+
+#output:
+#This is some underlines of numbers in Python
+#I will print "2_3"
+#23
+#I will print "3_2"
+#32
+#I will print "2_3.5"
+#23.5
+#I will print "3_2.5"
+#32.5
+#在Python中，下划线可以用来增强可读性，pyhton会忽略下划线
+
