1b70a1835b4754990711d02429fc1cbe93ff656b
John Forkens <fdmh2024@outlook.com>
2026-02-14 19:23:10 +0800

    Add files via upload

---
+++ practice/2.9_numbers_expression.py
@@ -0,0 +1,20 @@
+#Project: Python expressions
+#By: John Forkens Website:  https://github.com/john488640
+#E:\Project\python\Practice\2.9_numbers_expression
+#2.9 Numbers and Expressions
+#This program will practice the use of numbers and expressions in Python.
+
+print("我将会通过不同的表达式体现\"8\"")
+print(2+6)
+print(2*4)
+print(10-2)
+print(16/2)
+print("Tips:Pyhton中的除法运算结果是浮点数，即使被除数和除数都是整数。")
+
+#output:
+#我将会通过不同的表达式体现"8"
+#8
+#8
+#8
+#8.0
+#Tips:Python中的除法运算结果是浮点数，即使呗除数和除数都是整数。
