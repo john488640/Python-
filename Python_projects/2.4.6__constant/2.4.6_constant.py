f9357e367dc7840ec46af4425baf5fd54eea6643
John Forkens <fdmh2024@outlook.com>
2026-02-14 19:04:46 +0800

    Add files via upload

---
+++ Python_projects/2.4.6__constant/2.4.6_constant.py
@@ -0,0 +1,8 @@
+#Project: Python constants
+#By: John Forkens Website:  https://github.com/john488640
+#E:\Project\python\2.4.6_constant
+#2.4.6 P24-25
+#This program is to demonstrate the use of constants in Python.
+
+CONSTANT = 10 #大写字母表示常量
+print(CONSTANT)
