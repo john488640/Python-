b3a99899e81269d0c8b5ca57741a861e2a5c77f5
John Forkens <fdmh2024@outlook.com>
2026-02-14 19:03:59 +0800

    Add files via upload

---
+++ Python_projects/2.4.5_assign_values_to_multiple_variables_simultaneously/2.4.5_assign_values_to_multiple_variables_simultaneously.py
@@ -0,0 +1,10 @@
+#Project: Python assign values to multiple variables simultaneously
+#By: John Forkens Website:  https://github.com/john488640
+#E:\Project\python\2.4.5_assign_values_to_multiple_variables_simultaneously
+#2.4.5 P24
+#This program will assign values to multiple variables simultaneously.
+
+x, y, z = 1, 2, 3
+print(x, y, z)
+
+#Output: 1 2 3
