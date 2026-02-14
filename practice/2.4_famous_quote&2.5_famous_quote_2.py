b288ec3b3b32338d7184a502449e046016399578
John Forkens <fdmh2024@outlook.com>
2026-02-14 19:22:53 +0800

    Add files via upload

---
+++ practice/2.4_famous_quote&2.5_famous_quote_2.py
@@ -0,0 +1,21 @@
+#Project: Python famous quotes program
+#By: John Forkens Website:  https://github.com/john488640
+#E:\Project\python\Practice\2.4_famous_quote&2.5_famous_quote_2.py
+#P2.4 P21
+#This program will print a famous quote
+
+print("Design is not just what it looks like and feels like. Design is how it works. - Steve Jobs")
+#output:
+#Design is not just what it looks like and feels like. Design is how it works. - Steve Jobs
+
+#Project: Python famous quotes program
+#By: John Forkens Website:  https://github.com/john488640
+#E:\Project\python\Practice\2.4_famous_quote&2.5_famous_quote_2.py
+#P2.5 P21
+#This program will print a famous quote by value
+
+name = "Steve Jobs"
+quote = "Design is not just what it looks like and feels like. Design is how it works."
+print(f"{name} once said, '{quote}'")
+#output:
+#Steve Jobs once said, 'Design is not just what it looks like and feels like. Design is how it works.'
