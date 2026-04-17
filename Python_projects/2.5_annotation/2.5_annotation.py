18e36141bd56e6f8eff664f725484e2742cbba2d
John Forkens <fdmh2024@outlook.com>
2026-04-17 21:36:48 +0800

    Add files via upload

---
+++ Python_projects/2.5_annotation/2.5_annotation.py
@@ -0,0 +1,28 @@
+#Project: Python annotations
+#By: John Forkens Website:  https://github.com/john488640
+#E:\Project\python\2.5_annotation
+#2.5 P25
+#This program will show how to use the annotations in python
+
+#No.1
+
+#这条代码输出一些文字
+print("Hello World!")
+
+#No.2
+
+"""
+    这些代码算出同一个数
+    并且将结果相加
+"""
+
+print((2+3)+(3+2))
+
+#No.3
+#当不想执行一些代码时，可以在代码最前面加上井号
+
+#print("#")
+
+#output:
+#Hello World!
+#10
